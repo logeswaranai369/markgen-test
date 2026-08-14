3d2e9ddcacfcae75db13fd472f80203809481395
logeswaranai369 <logeswaranai369@gmail.com>
2026-08-14 02:34:06 -0700

    Delete nginx-24-04 directory

--- nginx-24-04/nginx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p {/usr/local/src/nginx-24-04/opt/cloudstack,/usr/local/src/nginx-24-04/}
-
-
-cd /usr/local/src/nginx-24-04/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/nginx-24-04/nginx.yaml
